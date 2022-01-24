84fc7f52f270bcdc11ea075dfa3593ff73ca9d55
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2022-01-24 12:29:48 +0300

    Update sortic.h

--- sortic.h
+++ sortic.h
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+void sa(vector <int>& a);
+void sb(vector <int>& b);
+
+void ss(vector <int> &a, vector <int> &b);
+void pb(vector <int> &a, vector <int> &b);
+
+void ra(vector <int>& a);
+void rb(vector <int>& b);
+void rr(vector <int> &a, vector <int> &b);
+
+void rra(vector <int>& a);
+void rrb(vector <int>& b);
+void rrr(vector <int> &a, vector <int> &b);
