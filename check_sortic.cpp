820de7b9ed050eb125d24cd5e21be7cd37c385b5
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2022-02-03 09:10:29 +0300

    Update check_sortic.cpp

--- check_sortic.cpp
+++ check_sortic.cpp
@@ -1,1 +1,24 @@
+#include "sortic.h"
 
+void run_programm(string str, vector <int> &a, vector <int> &b){
+    if (str == "sa")
+        sa(a);
+    else if (str == "sb")
+        sb(b);
+    else if (str == "pa")
+        pa(a, b);
+    else if (str == "pb")
+        pb(a, b);
+    else if (str == "ra")
+        ra(a);
+    else if (str == "rb")
+        rb(b);
+    else if (str == "rr")
+        rr(a, b);
+    else if (str == "rra")
+        rra(a);
+    else if (str == "rrb")
+        rrb(b);
+    else if (str == "rrr")
+        rrr(a, b);
+}
